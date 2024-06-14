a45f6dc6b31841ea42d37c7ac1873cfabdd3da89
uzn1023 <e5305@gm.kochi-ct.jp>
2024-06-14 19:42:54 +0900

    feat: accept multi urls

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ database_id = st.sidebar.text_input('データベースID', value=st.secrets['da
 st.title("論文要約GPT")
 
 uploaded_files = st.file_uploader("PDFファイルをアップロードしてください", type="pdf", accept_multiple_files=True)
-pdf_url = st.text_input("PDFファイルのURLを入力してください")
+pdf_urls = st.text_area("PDFファイルのURLを改行区切りで入力してください")
 
-if uploaded_files or pdf_url:
+if uploaded_files or pdf_urls:
     if st.button('処理を開始'):
         progress_bar = st.progress(0)
         if uploaded_files:
@@ -31,21 +31,23 @@ if uploaded_files or pdf_url:
                         st.error(f'{uploaded_file.name}の処理に失敗しました')
                         traceback.print_exc()
                 progress_bar.progress((i + 1) / len(uploaded_files))
-        elif pdf_url:
-            try:
-                response = requests.get(pdf_url)
-                response.raise_for_status()  # ステータスコードが200以外の場合、例外を発生させる
-                pdf_content = response.content
-                with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-                    temp_file.write(pdf_content)
-                    temp_file_path = temp_file.name
-                    dict = summarize_paper(temp_file_path, database_id, notion_api_key)
-                    with st.expander(f"{pdf_url}の要約"):
-                        st.table(dict)
-            except requests.exceptions.RequestException as e:
-                st.error(f"PDFファイルの取得に失敗しました: {e}")
-            except:
-                st.error(f'{pdf_url}の処理に失敗しました')
-                traceback.print_exc()
+        elif pdf_urls:
+            pdf_urls = pdf_urls.splitlines()
+            for i, pdf_url in enumerate(pdf_urls):
+                try:
+                    response = requests.get(pdf_url)
+                    response.raise_for_status()  # ステータスコードが200以外の場合、例外を発生させる
+                    pdf_content = response.content
+                    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+                        temp_file.write(pdf_content)
+                        temp_file_path = temp_file.name
+                        dict = summarize_paper(temp_file_path, database_id, notion_api_key)
+                        with st.expander(f"{pdf_url}の要約"):
+                            st.table(dict)
+                except requests.exceptions.RequestException as e:
+                    st.error(f"PDFファイルの取得に失敗しました: {e}")
+                except:
+                    st.error(f'{pdf_url}の処理に失敗しました')
+                    traceback.print_exc()
+                progress_bar.progress((i + 1) / len(pdf_urls))
         st.success('すべてのファイルが処理されました')
-
--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def summarize_paper(pdf_path,database_id,notion_api_key):
     summarized_texts = [entry['summarized_text'] for entry in summary.values()]
     concatenated_text = ' '.join(summarized_texts)
     first_page_text = summary[1]['text']
-    time.sleep(60)
+    #time.sleep(60)
 
     properties = get_properties_from_text(first_page_text)
 
     formated_paper = get_summarize_by_format_from_text(concatenated_text)
 
-    time.sleep(60)
+    #time.sleep(60)
 
     #images = summarize_pdf_image(doc)
 
